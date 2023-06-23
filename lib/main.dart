3467e2d4753b946ba0e9933347afbc6225191843
LaBlaugrana <aayush.dubey.21cse@bmu.edu.in>
2023-06-23 16:16:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:dashboard/profile/profile.dart';
+import 'package:dashboard/profile/update_profile.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
+}
+
+
+const profilepic = 'images/man.png';
